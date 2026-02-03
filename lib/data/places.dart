36d61a912c98b411c3296104ab62a0e36f769ea1
gevrey <gevrey@pobox.com>
2026-02-03 13:04:50 +0100

    Fix avalanche bulletin links (use www.meteofrance.com)

--- lib/data/places.dart
+++ lib/data/places.dart
@@ -7,8 +7,8 @@ import '../models/place.dart';
 /// - Snow report URLs are best-effort and may change.
 final places = <Place>[
   // Peaks
-  Place(id: 'mont-blanc', name: 'Mont Blanc', type: PlaceType.peak, lat: 45.8326, lon: 6.8652, avalancheReportUrl: Uri.parse('https://meteofrance.com/meteo-montagne/alpes-du-nord/haute-savoie/bulletin-avalanches')),
-  Place(id: 'aiguille-du-midi', name: 'Aiguille du Midi', type: PlaceType.peak, lat: 45.8799, lon: 6.8871, avalancheReportUrl: Uri.parse('https://meteofrance.com/meteo-montagne/alpes-du-nord/haute-savoie/bulletin-avalanches')),
+  Place(id: 'mont-blanc', name: 'Mont Blanc', type: PlaceType.peak, lat: 45.8326, lon: 6.8652, avalancheReportUrl: Uri.parse('https://www.meteofrance.com/meteo-montagne/alpes-du-nord/haute-savoie/bulletin-avalanches')),
+  Place(id: 'aiguille-du-midi', name: 'Aiguille du Midi', type: PlaceType.peak, lat: 45.8799, lon: 6.8871, avalancheReportUrl: Uri.parse('https://www.meteofrance.com/meteo-montagne/alpes-du-nord/haute-savoie/bulletin-avalanches')),
   Place(id: 'dents-du-midi', name: 'Dents du Midi', type: PlaceType.peak, lat: 46.1679, lon: 6.8420, avalancheReportUrl: Uri.parse('https://whiterisk.ch/en/avalanche-bulletin')),
   const Place(id: 'le-saleve', name: 'Le Salève', type: PlaceType.peak, lat: 46.1167, lon: 6.1833),
 
@@ -20,7 +20,7 @@ final places = <Place>[
     lat: 45.9237,
     lon: 6.8694,
     snowReportUrl: Uri.parse('https://www.chamonix.com/en/conditions'),
-    avalancheReportUrl: Uri.parse('https://meteofrance.com/meteo-montagne/alpes-du-nord/haute-savoie/bulletin-avalanches'),
+    avalancheReportUrl: Uri.parse('https://www.meteofrance.com/meteo-montagne/alpes-du-nord/haute-savoie/bulletin-avalanches'),
   ),
   Place(
     id: 'les-houches',
@@ -29,7 +29,7 @@ final places = <Place>[
     lat: 45.8905,
     lon: 6.7952,
     snowReportUrl: Uri.parse('https://www.leshouches.com/en/winter/ski-area/snow-report'),
-    avalancheReportUrl: Uri.parse('https://meteofrance.com/meteo-montagne/alpes-du-nord/haute-savoie/bulletin-avalanches'),
+    avalancheReportUrl: Uri.parse('https://www.meteofrance.com/meteo-montagne/alpes-du-nord/haute-savoie/bulletin-avalanches'),
   ),
   Place(
     id: 'megeve',
@@ -38,7 +38,7 @@ final places = <Place>[
     lat: 45.8568,
     lon: 6.6180,
     snowReportUrl: Uri.parse('https://www.megeve.com/en/infos-live/webcams-weather-snow/'),
-    avalancheReportUrl: Uri.parse('https://meteofrance.com/meteo-montagne/alpes-du-nord/haute-savoie/bulletin-avalanches'),
+    avalancheReportUrl: Uri.parse('https://www.meteofrance.com/meteo-montagne/alpes-du-nord/haute-savoie/bulletin-avalanches'),
   ),
   Place(
     id: 'la-clusaz',
@@ -47,7 +47,7 @@ final places = <Place>[
     lat: 45.9059,
     lon: 6.4246,
     snowReportUrl: Uri.parse('https://www.laclusaz.com/en/live/snow-report/'),
-    avalancheReportUrl: Uri.parse('https://meteofrance.com/meteo-montagne/alpes-du-nord/haute-savoie/bulletin-avalanches'),
+    avalancheReportUrl: Uri.parse('https://www.meteofrance.com/meteo-montagne/alpes-du-nord/haute-savoie/bulletin-avalanches'),
   ),
   Place(
     id: 'avoriaz',
@@ -56,7 +56,7 @@ final places = <Place>[
     lat: 46.1926,
     lon: 6.7753,
     snowReportUrl: Uri.parse('https://en.avoriaz.com/ski-area/snow-report/'),
-    avalancheReportUrl: Uri.parse('https://meteofrance.com/meteo-montagne/alpes-du-nord/haute-savoie/bulletin-avalanches'),
+    avalancheReportUrl: Uri.parse('https://www.meteofrance.com/meteo-montagne/alpes-du-nord/haute-savoie/bulletin-avalanches'),
   ),
   Place(
     id: 'morzine',
@@ -65,7 +65,7 @@ final places = <Place>[
     lat: 46.1800,
     lon: 6.7050,
     snowReportUrl: Uri.parse('https://www.morzine-avoriaz.com/en/ski/snow-report'),
-    avalancheReportUrl: Uri.parse('https://meteofrance.com/meteo-montagne/alpes-du-nord/haute-savoie/bulletin-avalanches'),
+    avalancheReportUrl: Uri.parse('https://www.meteofrance.com/meteo-montagne/alpes-du-nord/haute-savoie/bulletin-avalanches'),
   ),
   Place(
     id: 'verbier',
